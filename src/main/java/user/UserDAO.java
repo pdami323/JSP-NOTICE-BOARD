c007fe45aeb340ed3788f70b139449d6f4ccf2a2
Dami Park <ekal0323@gmail.com>
2022-11-01 17:28:02 +0900

    Update Login Function

---
+++ src/main/java/user/UserDAO.java
@@ -0,0 +1,46 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+//데이터베이스 접근 객체로서 데이터베이스에서 정보를 불러오거나 값을 저장하는 역할
+public class UserDAO {
+	
+	private Connection conn;	//데이터베이스에 접근하게 해주는 객체
+	private PreparedStatement pstat;
+	private ResultSet rs;
+	
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/BBS?serverTimezone=UTC";	//3306포트는 mysql 서버 
+			String dbID = "root";
+			String dbPassword = "ekal0323";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
+		try {
+			pstat = conn.prepareStatement(SQL);	
+			pstat.setString(1, userID);		//SQL에 ?대신에 매개변수로서 userID를 넣어줌으로써 해킹을 방지
+			rs = pstat.executeQuery();		//SQL 실행 결과 
+			if(rs.next()) {					//실행 결과가 존재한다면
+				if(rs.getString(1).equals(userPassword)) {		//비밀번호 확인
+					return 1;	//로그인 성공
+				}
+				else
+					return 0;		//비밀번호 불일치
+			}
+			return -1;						//결과값이 없다면, 즉 회원이 없는 경우
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -2;	//데이터베이스 오류를 의미
+	}
+}
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -43,7 +43,7 @@
 		<div class = "col-lg-4"></div>
 		<div class = "col-lg-4">
 			<div class = "jumbotron" style = "padding-top : 20px;">
-				<form method = "post" action = "loginAction.jsp">	<!-- loginAction.jsp로 post방식을 이용하여 정보를 보내겠다는 의미 -->
+				<form method = "post" action = "loginAction.jsp">	<!-- loginAction.jsp로 post방식을 이용하여 정보를 보내겠다는 의미로 로그인 결과 화면 -->
 					<h3 style = "text-align : center;">로그인 화면</h3>
 					<div class = "form-group">
 						<input type = "text" class = "form-control" placeholder = "아이디" name = "userID" maxlength = "20">
